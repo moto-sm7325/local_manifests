65c9304f4b8b6ded2c1045c450688ef80339ab90
Marc Bourgoin <themard85@gmail.com>
2026-02-10 13:35:32 -0700

    Update for lineage-23.2

--- dubai_dirty_build.sh
+++ dubai_dirty_build.sh
@@ -4,24 +4,25 @@
 
 # if you want to build without using ccache, comment
 # the next 5 lines
-# export USE_CCACHE=1
-# export CCACHE_DIR=~/mnt/.ccache
-# export CCACHE_MAX_SIZE=500G
-# export CCACHE_EXEC=$(which ccache)
-# ccache -M $CCACHE_MAX_SIZE
+#export USE_CCACHE=1
+#export CCACHE_DIR=/mnt/.ccache
+#export CCACHE_MAX_SIZE=300G
+#export CCACHE_EXEC=$(which ccache)
+#ccache -M $CCACHE_MAX_SIZE
 
 # we want all compiler messages in English
 export LANGUAGE=C
 
 # set up the environment (variables and functions)
 source build/envsetup.sh
+breakfast lineage_dubai-bp4a-userdebug
 
-# clean the out dir; comment out, if you want to do
-# a dirty build
-# make -j9 ARCH=arm clean
-breakfast lineage_dubai-bp2a-userdebug
+# clean build
+# make clean
+
+# dirty build
 make installclean
 
 # fire up the building process and also log stdout
 # and stderrout
-brunch lineage_dubai-bp2a-userdebug 2>&1 | tee dubai_make.log
+brunch lineage_dubai-bp4a-userdebug 2>&1 | tee dubai_make.log
--- xpeng_dirty_build.sh
+++ xpeng_dirty_build.sh
@@ -4,23 +4,25 @@
 
 # if you want to build without using ccache, comment
 # the next 5 lines
-# export USE_CCACHE=1
-# export CCACHE_DIR=~/mnt/.ccache
-# export CCACHE_MAX_SIZE=500G
-# export CCACHE_EXEC=$(which ccache)
-# ccache -M $CCACHE_MAX_SIZE
+#export USE_CCACHE=1
+#export CCACHE_DIR=/mnt/.ccache
+#export CCACHE_MAX_SIZE=300G
+#export CCACHE_EXEC=$(which ccache)
+#ccache -M $CCACHE_MAX_SIZE
 
 # we want all compiler messages in English
 export LANGUAGE=C
 
 # set up the environment (variables and functions)
 source build/envsetup.sh
+breakfast lineage_xpeng-bp4a-userdebug
 
-# clean the out dir; comment out, if you want to do
-# a dirty build
-breakfast lineage_xpeng-bp2a-userdebug
+# clean build
+# make clean
+
+# dirty build
 make installclean
 
 # fire up the building process and also log stdout
 # and stderrout
-brunch lineage_xpeng-bp2a-userdebug 2>&1 | tee xpeng_make.log
+brunch lineage_xpeng-bp4a-userdebug 2>&1 | tee xpeng_make.log
